807288a894308101854eecfbc982abbd15cd9f68
Sealkeen <lettersforlotro@gmail.com>
2023-11-08 17:03:51 +0300

    Added files (extracted from main MediaStreamer repo)

---
+++ LogLevel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediaStreamer.Logging
+{
+    public enum LogLevel
+    {
+        Trace = 1,
+        Debug = 2,
+        Info = 3,
+        Error = 4
+    }
+}
